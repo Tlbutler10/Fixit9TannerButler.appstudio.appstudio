b4f89623c36aa6c789ce59be66df5b3ad7d60711
Tanner <tannerbutler15@gmail.com>
2019-10-07 19:13:28 -0500

    updated

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -2,3 +2,13 @@
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
 
+
+Button1.onclick=function(){
+  if (members.includes(inptFirstName.value)){
+      NSB.MsgBox("That name already exists") 
+    } else {
+      members.push(inptFirstName.value)
+      NSB.MsgBox("That name has been added")
+      console.log(members)
+}
+}
